6af1072b1bca0bcfd16527984914029dc4f3452d
MiMo <michael.a.morris.public@gmail.com>
2021-02-09 22:23:41 -0600

    feat: Add grid template areas functionality
    Install and configure grid template areas for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,20 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    extend: {
+      gridTemplateAreas: {
+        layout: ['nav header', 'nav main', 'footer footer']
+      },
+      gridTemplateColumns: {
+        layout: '200px 1fr'
+      },
+      gridTemplateRows: {
+        layout: '100px 1fr 100px'
+      }
+    }
   },
   variants: {
     extend: {}
   },
-  plugins: []
+  plugins: [require('@savvywombat/tailwindcss-grid-areas')]
 }
