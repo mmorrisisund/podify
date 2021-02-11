3589d30cf2aa8dd10ea819e00db1faa26d9038b9
MiMo <michael.a.morris.public@gmail.com>
2021-02-10 21:13:24 -0600

    feat: 🔥 Remove use of css grid
    Remove grid areas and plugin support

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,10 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      gridTemplateAreas: {
-        layout: ['nav header', 'nav main', 'footer footer']
-      },
-      gridTemplateColumns: {
-        layout: '200px 1fr'
-      },
-      gridTemplateRows: {
-        layout: '100px 1fr 100px'
-      }
-    }
+    extend: {}
   },
   variants: {
     extend: {}
   },
-  plugins: [require('@savvywombat/tailwindcss-grid-areas')]
+  plugins: []
 }
