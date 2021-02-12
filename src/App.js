367be5e97b43525ab90ff954fb7d786479b33295
MiMo <michael.a.morris.public@gmail.com>
2021-02-11 18:33:05 -0600

    feat: Add split pane resizer to general layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import SplitPane from './components/SplitPane/SplitPane'
+
 function App () {
-  return <div></div>
+  return (
+    <div className='flex flex-col min-h-screen'>
+      <SplitPane className='flex-1'>
+        <SplitPane.Left className='grid-in-nav'>
+          <nav className='h-full bg-blue-500' />
+        </SplitPane.Left>
+        <SplitPane.Right className='grid-in-main'>
+          <header className='h-24 bg-red-500 grid-in-header' />
+          <main className='h-full bg-green-500'></main>
+        </SplitPane.Right>
+      </SplitPane>
+      <footer className='h-24 bg-yellow-500' />
+    </div>
+  )
 }
 
 export default App
