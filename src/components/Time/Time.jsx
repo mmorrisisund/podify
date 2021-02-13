bc313cd3686f84dd5f7fa5afc9097f8ad543e113
MiMo <michael.a.morris.public@gmail.com>
2021-02-12 20:08:16 -0600

    feat: Initial Commit - Time
    Component to format times

---
+++ src/components/Time/Time.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react'
+
+export const Time = ({ seconds, ...props }) => {
+  const [secondsLeft, setSecondsLeft] = useState(0)
+  const [minutesLeft, setMinutesLeft] = useState(0)
+  const [hoursLeft, setHoursLeft] = useState(0)
+
+  useEffect(() => {
+    const h = Math.floor(seconds / 3600)
+    const m = Math.floor((seconds % 3600) / 60)
+    const s = Math.floor(seconds % 60)
+
+    setSecondsLeft(s)
+    setMinutesLeft(m)
+    setHoursLeft(h)
+  }, [seconds])
+
+  const formatHours = h => (h ? (h < 1 ? '' : String(h).padStart(2, 0)) : '')
+  const formatTime = t => (t ? String(t).padStart(2, 0) : '00')
+
+  return (
+    <div {...props}>
+      <span className='select-none'>{formatHours(hoursLeft)}</span>
+      {hoursLeft > 0 && <span>:</span>}
+      <span className='select-none'>{formatTime(minutesLeft)}</span>
+      <span className='select-none'>:</span>
+      <span className='select-none'>{formatTime(secondsLeft)}</span>
+    </div>
+  )
+}
