80c83bb38f05c0022bf153bc0d23ec4a5676b242
MiMo <michael.a.morris.public@gmail.com>
2021-02-14 11:07:26 -0600

    feat: ✨ Initial Commit - MainView
    Initial layout, routing, and static functionality

---
+++ src/components/MainView/MainView.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect, useRef } from 'react'
+import { Switch, Route } from 'react-router-dom'
+
+import { Topbar } from '../Topbar'
+import { Home, Search, Library, Podcast } from '../../pages'
+
+export const MainView = () => {
+  const mainRef = useRef()
+  const topRef = useRef()
+  const [opacity, setOpacity] = useState(0)
+
+  useEffect(() => {
+    const refCopy = mainRef
+    const handleScroll = e => {
+      if (
+        topRef.current.clientHeight * 2 < mainRef.current.scrollTop &&
+        opacity >= 1
+      ) {
+        return
+      }
+
+      setOpacity(mainRef.current.scrollTop / (topRef.current.clientHeight * 2))
+    }
+
+    mainRef.current?.addEventListener('scroll', handleScroll)
+
+    return () => refCopy.current?.removeEventListener('scroll', handleScroll)
+  }, [mainRef, opacity])
+
+  return (
+    <div className='relative'>
+      <Topbar ref={topRef} backgroundOpacity={opacity} />
+      <main
+        ref={mainRef}
+        className='h-screen py-24 overflow-auto bg-true-gray-500'
+      >
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/search' component={Search} />
+          <Route path='/library' component={Library} />
+          <Route path='/podcast/:podcastId' component={Podcast} />
+        </Switch>
+      </main>
+    </div>
+  )
+}
