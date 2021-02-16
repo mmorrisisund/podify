e9e5e095097d0a2f74c85c7c3e504606aacc1972
MiMo <michael.a.morris.public@gmail.com>
2021-02-15 21:26:05 -0600

    feat: Add now playing footer and sidenav
    Add new components, adjust Splitpane for new api

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import SplitPane from './components/SplitPane/SplitPane'
+import { NowPlayingBar } from './components/NowPlayingBar'
+import { Navigation } from './components/Navigation'
+import { MainLogo } from './components/MainLogo/MainLogo'
+import { MainView } from './components/MainView'
 
 function App () {
   return (
-    <div className='flex flex-col min-h-screen'>
-      <SplitPane className='flex-1'>
-        <SplitPane.Left className='grid-in-nav'>
-          <nav className='h-full bg-blue-500' />
-        </SplitPane.Left>
-        <SplitPane.Right className='grid-in-main'>
-          <header className='h-24 bg-red-500 grid-in-header' />
-          <main className='h-full bg-green-500'></main>
-        </SplitPane.Right>
+    <div className='flex flex-col'>
+      <SplitPane className='' min={175} max={400}>
+        <nav className='h-full bg-black'>
+          <MainLogo className='p-6' />
+          <Navigation />
+        </nav>
+        <MainView />
       </SplitPane>
-      <footer className='h-24 bg-yellow-500' />
+
+      <footer className='fixed inset-x-0 bottom-0 h-24'>
+        <NowPlayingBar />
+      </footer>
     </div>
   )
 }
