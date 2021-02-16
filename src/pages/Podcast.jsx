f6dc1354b23d110a386ce28e6ead7d2c26e952dd
MiMo <michael.a.morris.public@gmail.com>
2021-02-15 21:23:16 -0600

    feat:✨ Add new components to page
    Main header, action bar (wip), and episode listing

--- src/pages/Podcast.jsx
+++ src/pages/Podcast.jsx
@@ -1,8 +1,41 @@
-export const Podcast = () => (
-  <>
-    <div className='h-96'>Podcast 1</div>
-    <div className='h-96'>Podcast 2</div>
-    <div className='h-96'>Podcast 3</div>
-    <div className='h-96'>Podcast 4</div>
-  </>
-)
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { PodcastHeader } from '../components/Podcast'
+import { ActionBar } from '../components/Podcast/ActionBar'
+import { EpisodeList } from '../components/Podcast/EpisodeList'
+import { getPodcastById } from '../utils/itunesApi'
+
+export const Podcast = () => {
+  const [podcast, setPodcast] = useState(null)
+  const [error, setError] = useState(false)
+  const { podcastId } = useParams()
+
+  useEffect(() => {
+    const fetchPodcast = async () => {
+      try {
+        const podcast = await getPodcastById(podcastId)
+        setPodcast(podcast)
+      } catch (error) {
+        setError(true)
+      }
+    }
+    fetchPodcast()
+  }, [podcastId])
+
+  return (
+    <div className='bg-true-gray-900'>
+      {error && <h1>An error occurred.</h1>}
+      <div className='-mt-14'>
+        {podcast && (
+          <PodcastHeader
+            image={podcast.image.url}
+            name={podcast.collectionName}
+            artist={podcast.artistName}
+          />
+        )}
+        <ActionBar />
+        <EpisodeList episodes={podcast?.items} />
+      </div>
+    </div>
+  )
+}
