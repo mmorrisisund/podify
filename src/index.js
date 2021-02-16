d6c916595a0a9f6a4d84561a126af0abd6904851
MiMo <michael.a.morris.public@gmail.com>
2021-02-15 21:26:30 -0600

    feat: Add new providers
    Wrap app with providers for auth and search

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import { BrowserRouter as Router } from 'react-router-dom'
 
 import './index.css'
 import App from './App'
+import { AuthProvider } from './context/AuthContext'
+import { SearchProvider } from './context/SearchContext'
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <AuthProvider>
+        <SearchProvider>
+          <App />
+        </SearchProvider>
+      </AuthProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
