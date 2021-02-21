032e41733b0de36c49bdfdeb74a77af261f46626
MiMo <michael.a.morris.public@gmail.com>
2021-02-21 16:27:56 -0600

    feat: add stylingto play button
    add scaling on hover and active state

--- src/components/Buttons/Play.jsx
+++ src/components/Buttons/Play.jsx
@@ -1,7 +1,10 @@
 import { Play } from '../Icons'
 
 export const PlayButton = ({ onPlay, className }) => (
-  <button className={className} onClick={onPlay}>
+  <button
+    className={`transition-transform hover:scale-105 active:scale-95 duration-75 focus:outline-none  ${className}`}
+    onClick={onPlay}
+  >
     <Play />
   </button>
 )
--- src/components/Buttons/index.js
+++ src/components/Buttons/index.js
@@ -1,3 +1,4 @@
 export * from './Submit'
 export * from './Cancel'
 export * from './Follow'
+export * from './Play'
