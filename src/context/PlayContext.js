626cfaf7cbd62989ea5599c9efb182718b6c36ae
MiMo <michael.a.morris.public@gmail.com>
2021-02-21 12:25:40 -0600

    feat: Add queue functionality
    Add actions and state to play list of episodes

--- src/context/PlayContext.js
+++ src/context/PlayContext.js
@@ -1,21 +1,41 @@
 import { createContext, useContext, useReducer } from 'react'
 
-export const PlayContext = createContext()
+export const PlayContext = createContext([])
 
-const SET_PLAYING = 'SET_PLAYING'
-const SET_EPISODES = 'SET_EPISODES'
-const SET_PODCAST = 'SET_PODCAST'
+export const actions = {
+  SET_PLAYING: 'SET_PLAYING',
+  SET_PODCAST: 'SET_PODCAST',
+  QUEUE_ADD: 'QUEUE_ADD',
+  QUEUE_REMOVE: 'QUEUE_REMOVE',
+  QUEUE_FORWARD: 'QUEUE_FORWARD',
+  QUEUE_REVERSE: 'QUEUE_REVERSE',
+  QUEUE_EMPTY: 'QUEUE_EMPTY',
+  QUEUE_PODCAST: 'QUEUE_PODCAST',
+  QUEUE_PLAY: 'QUEUE_PLAY'
+}
 
 function reducer (state, { type, payload }) {
   switch (type) {
-    case SET_PLAYING: {
+    case actions.SET_PLAYING: {
       return { ...state, currentEpisode: payload }
     }
-    case SET_EPISODES: {
-      return { ...state, episodes: payload }
-    }
-    case SET_PODCAST: {
+    case actions.SET_PODCAST: {
       return { ...state, podcast: payload }
+    }
+    case actions.QUEUE_ADD: {
+      return addToQueue(state, payload)
+    }
+    case actions.QUEUE_REMOVE: {
+      return removeFromQueue(state, payload)
+    }
+    case actions.QUEUE_EMPTY: {
+      return { ...state, queue: [] }
+    }
+    case actions.QUEUE_PODCAST: {
+      return { ...state, queue: payload }
+    }
+    case actions.QUEUE_PLAY: {
+      return { ...state, currentEpisode: state.queue[0] }
     }
     default: {
       return state
@@ -24,16 +44,16 @@ function reducer (state, { type, payload }) {
 }
 
 const initialState = {
-  currentEpisode: {},
-  episodes: [],
-  podcast: {}
+  currentEpisode: null,
+  podcast: null,
+  queue: []
 }
 
 export const PlayProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState)
 
   return (
-    <PlayContext.Provider value={[state, dispatch]}>
+    <PlayContext.Provider value={[state, dispatch, actions]}>
       {children}
     </PlayContext.Provider>
   )
@@ -45,4 +65,20 @@ export const usePlayContext = () => {
   if (!value) throw Error('usePlayContext must be wrapped in a provider')
 
   return value
+}
+
+function addToQueue (state, episode) {
+  const { queue } = state
+  const existing = queue.find(u => u === episode.enclosure.url)
+
+  if (existing) return
+
+  return { ...state, queue: [...queue, episode] }
+}
+
+function removeFromQueue (state, episode) {
+  const newQueue = state.queue.filter(
+    ep => ep.enclosure.url !== episode.enclosure.url
+  )
+  return { ...state, queue: newQueue }
 }
