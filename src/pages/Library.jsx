3561c71c7580f881d40e694f48a7a9327ff0a583
MiMo <michael.a.morris.public@gmail.com>
2021-02-21 12:29:39 -0600

    feat: ✨ Initial Commit - Library page

--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -1,8 +1,37 @@
-export const Library = () => (
-  <>
-    <div className='h-96'>Library 1</div>
-    <div className='h-96'>Library 2</div>
-    <div className='h-96'>Library 3</div>
-    <div className='h-96'>Library 4</div>
-  </>
-)
+import { useEffect, useState } from 'react'
+import { PodcastCard } from '../components/PodcastCard'
+import { useAuthContext } from '../context/AuthContext'
+import { getPodcastById } from '../utils/itunesApi'
+
+export const Library = () => {
+  const { user } = useAuthContext()
+  const [podcasts, setPodcasts] = useState([])
+
+  useEffect(() => {
+    async function getPodcasts (urls) {
+      const promises = await Promise.allSettled(urls.map(getPodcastById))
+      const podcasts = promises.reduce((acc, promise) => {
+        if (promise.status === 'fulfilled') {
+          return [...acc, promise.value]
+        }
+        return acc
+      }, [])
+      setPodcasts(podcasts)
+    }
+    getPodcasts(user.library)
+  }, [user])
+
+  return (
+    <div className='flex min-h-screen px-8 py-24 bg-true-gray-900'>
+      {podcasts.map(p => (
+        <PodcastCard
+          key={p.collectionId}
+          id={p.collectionId}
+          author={p.artistName}
+          title={p.collectionName}
+          image={p.image.url}
+        />
+      ))}
+    </div>
+  )
+}
