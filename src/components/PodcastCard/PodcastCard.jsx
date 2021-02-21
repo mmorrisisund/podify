7dcb661140ad689dc1d3469f96007a074bf03bf5
MiMo <michael.a.morris.public@gmail.com>
2021-02-21 16:29:22 -0600

    feat: add ability to play podcast from card
    library cards can begin playing podcast

--- src/components/PodcastCard/PodcastCard.jsx
+++ src/components/PodcastCard/PodcastCard.jsx
@@ -1,8 +1,13 @@
 import { Link } from 'react-router-dom'
+import { PlayButton } from '../Buttons'
 
-export const PodcastCard = ({ id, image, title, author }) => {
+export const PodcastCard = ({ id, image, title, author, onPlay }) => {
+  const handleOnPlay = e => {
+    e.preventDefault()
+    onPlay?.(id)
+  }
   return (
-    <div className='w-56 m-8 transition-colors rounded bg-true-gray-800 h-72 hover:bg-true-gray-700'>
+    <div className='relative w-56 m-8 transition-colors duration-300 rounded group bg-true-gray-800 h-72 hover:bg-true-gray-700'>
       <Link to={`/podcasts/${id}`}>
         <div className='flex flex-col p-4'>
           <img className='w-48 h-48 rounded-lg' src={image} alt={title} />
@@ -12,6 +17,12 @@ export const PodcastCard = ({ id, image, title, author }) => {
             </h3>
             <span className='text-xs text-true-gray-300'>{author}</span>
           </div>
+        </div>
+        <div className='absolute bottom-24 right-8'>
+          <PlayButton
+            className='p-4 transition duration-300 transform translate-y-4 rounded-full opacity-0 h-14 w-14 bg-lime-600 text-lime-50 group-hover:opacity-100 group-hover:translate-y-0'
+            onPlay={handleOnPlay}
+          />
         </div>
       </Link>
     </div>
--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -2,10 +2,17 @@ import { useEffect, useState } from 'react'
 import { PodcastCard } from '../components/PodcastCard'
 import { useAuthContext } from '../context/AuthContext'
 import { getPodcastById } from '../utils/itunesApi'
+import {
+  clearQueue,
+  addPodcastEpisodesToQueue,
+  playQueue,
+  usePlayContext
+} from '../context/PlayContext'
 
 export const Library = () => {
   const { user } = useAuthContext()
   const [podcasts, setPodcasts] = useState([])
+  const [, dispatch] = usePlayContext()
 
   useEffect(() => {
     async function getPodcasts (urls) {
@@ -18,8 +25,20 @@ export const Library = () => {
       }, [])
       setPodcasts(podcasts)
     }
-    getPodcasts(user.library)
+    if (user?.library) {
+      getPodcasts(user.library)
+    }
   }, [user])
+
+  const handleOnPlay = id => {
+    const podcast = podcasts.find(p => p.collectionId === id)
+
+    if (!podcast) return
+
+    dispatch(clearQueue())
+    dispatch(addPodcastEpisodesToQueue(podcast))
+    dispatch(playQueue())
+  }
 
   return (
     <div className='flex min-h-screen px-8 py-24 bg-true-gray-900'>
@@ -30,6 +49,7 @@ export const Library = () => {
           author={p.artistName}
           title={p.collectionName}
           image={p.image.url}
+          onPlay={handleOnPlay}
         />
       ))}
     </div>
