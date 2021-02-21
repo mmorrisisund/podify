bf060f280f8cd165a2d3bbd0bd96edfe9110d9be
MiMo <michael.a.morris.public@gmail.com>
2021-02-21 12:12:49 -0600

    feat: Initial Commit - Actionbar
    Move to separate component folder

---
+++ src/components/ActionBar/ActionBar.jsx
@@ -0,0 +1,37 @@
+import { FollowButton } from '../Buttons'
+import { useAuthContext } from '../../context/AuthContext'
+import { PlayButton } from '../Buttons/Play'
+import { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
+import { usePlayContext } from '../../context/PlayContext'
+
+export const ActionBar = ({ onFollow }) => {
+  const [isFollowing, setIsFollowing] = useState()
+  const location = useLocation()
+  const { user } = useAuthContext()
+  const [{ podcast }, dispatch, actions] = usePlayContext()
+
+  useEffect(() => {
+    const paths = location.pathname.split('/')
+    const id = paths[paths.length - 1]
+    const following = user.library.find(podcast => podcast === id) !== undefined
+    setIsFollowing(following)
+  }, [location, user.library])
+
+  const handlePlayClick = () => {
+    dispatch({ type: actions.QUEUE_PODCAST, payload: podcast.items })
+    dispatch({ type: actions.QUEUE_PLAY })
+  }
+
+  return (
+    <div className='flex items-center h-24 px-8 bg-red-600 bg-gradient-to-t from-true-gray-900 to-semi-transparent'>
+      <div className='flex items-center'>
+        <PlayButton
+          className='p-4 mr-10 rounded-full bg-lime-600 h-14 w-14 text-green-50'
+          onPlay={handlePlayClick}
+        />
+        {user && <FollowButton isFollowing={isFollowing} onClick={onFollow} />}
+      </div>
+    </div>
+  )
+}
--- src/components/Podcast/ActionBar.jsx
+++
@@ -1,7 +0,0 @@
-export const ActionBar = () => {
-  return (
-    <div className='h-24 bg-red-600 bg-gradient-to-t from-true-gray-900 to-semi-transparent'>
-      <h4 className='text-xl text-white'>Action Bar</h4>
-    </div>
-  )
-}
