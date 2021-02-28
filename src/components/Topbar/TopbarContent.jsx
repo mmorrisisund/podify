41542c8b628eeec8a6fa5541ecee49f511e0b193
MiMo <michael.a.morris.public@gmail.com>
2021-02-28 15:07:28 -0600

    refactor: change to search method
    begin using url parameters for search term and searching with spotify

--- src/components/Topbar/TopbarContent.jsx
+++ src/components/Topbar/TopbarContent.jsx
@@ -1,11 +1,11 @@
 import { Switch, Route } from 'react-router-dom'
-import { SearchInput } from '../SearchInput'
+import { SearchBar } from '../SearchInput'
 
 export const TopbarContent = () => {
   return (
     <div className='flex-1 ml-4 pointer-events-auto'>
       <Switch>
-        <Route path='/search' component={SearchInput} />
+        <Route path='/search' component={SearchBar} />
       </Switch>
     </div>
   )
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,3 +1,29 @@
+import { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
+
+import { search, debounce } from '../utils'
+import { PodcastCard } from '../components/PodcastCard'
+
+const doSearch = debounce(async function doSearch (term, cb) {
+  const result = await search(term)
+  cb(result)
+})
+
 export const Search = () => {
-  return <div className='min-h-full bg-true-gray-900'></div>
+  const [result, setResult] = useState()
+  const location = useLocation()
+
+  useEffect(() => {
+    if (location.pathname !== '/search' && location.pathname !== '/search/') {
+      doSearch(location.pathname.slice(8), setResult)
+    }
+  }, [location.pathname])
+
+  return (
+    <div className='flex flex-wrap min-h-full py-24 bg-true-gray-900'>
+      {result?.shows?.items?.map?.(show => (
+        <PodcastCard key={show.id} podcast={show} />
+      ))}
+    </div>
+  )
 }
