1d69aea6097e886078e8e268151e7eb7b0b25012
MiMo <michael.a.morris.public@gmail.com>
2021-02-28 15:12:26 -0600

    chore: misc trivial changes
    temporarily remove categories and export for auth callback page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,10 @@ import { Category } from '../components/Category'
 export const Home = () => {
   return (
     <div className='pt-24 bg-true-gray-900'>
-      <Category term='sports' />
+      {/* <Category term='sports' />
       <Category term='comedy' />
       <Category term='true crime' />
-      <Category term='politics' />
+      <Category term='politics' /> */}
     </div>
   )
 }
