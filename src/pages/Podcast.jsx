8258db62dbeb5113f89407860bc8a8ae4359c0e6
MiMo <michael.a.morris.public@gmail.com>
2021-02-28 15:11:44 -0600

    refactor: switch to using spotify api
    cleaned up data access, and adjust for new data shape

--- src/pages/Podcast.jsx
+++ src/pages/Podcast.jsx
@@ -1,57 +1,33 @@
-import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { PodcastHeader } from '../components/Podcast'
+import { PodcastHeader } from '../components/PodcastHeader'
 import { ActionBar } from '../components/ActionBar'
-import { EpisodeList } from '../components/Podcast/EpisodeList'
-import { usePlayContext } from '../context/PlayContext'
-import { useAuth } from '../providers/auth'
-import { getPodcastById } from '../utils/itunesApi'
+import { EpisodeList } from '../components/EpisodeList/EpisodeList'
 
-export const Podcast = () => {
-  const [{ podcast }, dispatch, actions] = usePlayContext()
-  const [error, setError] = useState(false)
+import { usePodcast } from '../hooks/queries'
+
+export const Podcast = ({ mainRef }) => {
   const { podcastId } = useParams()
-  const { user, setUser } = useAuth()
+  const { data: podcast, isError, isLoading } = usePodcast(podcastId)
 
-  useEffect(() => {
-    const fetchPodcast = async () => {
-      try {
-        const podcast = await getPodcastById(podcastId)
-        dispatch({ type: actions.SET_PODCAST, payload: podcast })
-      } catch (error) {
-        setError(true)
-      }
-    }
-    fetchPodcast()
-  }, [podcastId, dispatch, actions.SET_PODCAST])
-
-  const handleOnFollow = () => {
-    const existing = user.library.find(p => p === podcastId)
-
-    if (existing) {
-      setUser({
-        ...user,
-        library: [...user.library.filter(p => p !== podcastId)]
-      })
-    } else {
-      setUser({ ...user, library: [...user.library, podcastId] })
-    }
-  }
+  // const [{ podcast }, dispatch, actions] = usePlayContext()
+  // const { user, setUser } = useAuth()
 
   return (
     <div className='bg-true-gray-900'>
-      {error && <h1>An error occurred.</h1>}
-      <div className='-mt-14'>
-        {podcast && (
-          <PodcastHeader
-            image={podcast.image.url}
-            name={podcast.collectionName}
-            artist={podcast.artistName}
-          />
-        )}
-        <ActionBar onFollow={handleOnFollow} />
-        <EpisodeList episodes={podcast?.items} />
-      </div>
+      {isError && <h1>An error occurred.</h1>}
+      {!isLoading && (
+        <div className='-mt-14'>
+          {podcast && (
+            <PodcastHeader
+              image={podcast.images[0].url}
+              name={podcast.name}
+              artist={podcast.publisher}
+            />
+          )}
+          <ActionBar />
+          <EpisodeList podcastId={podcastId} mainRef={mainRef} />
+        </div>
+      )}
     </div>
   )
 }
