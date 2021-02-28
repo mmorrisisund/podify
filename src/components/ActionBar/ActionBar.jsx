ebcb7053e4cf1c351e0d440e4216c36f7d42fbe9
MiMo <michael.a.morris.public@gmail.com>
2021-02-28 14:39:55 -0600

    refactor: add react query for follow status
    update status of following

--- src/components/ActionBar/ActionBar.jsx
+++ src/components/ActionBar/ActionBar.jsx
@@ -1,30 +1,34 @@
+import { useMutation, useQueryClient } from 'react-query'
+
 import { FollowButton } from '../Buttons'
-import { useAuth } from '../../providers/auth'
 import { PlayButton } from '../Buttons/Play'
-import { useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom'
-import { usePlayContext } from '../../context/PlayContext'
+import { useParams } from 'react-router-dom'
+// import { usePlayContext } from '../../context/PlayContext'
+import { useFollowStatus } from '../../hooks/queries'
+import { addUserShows, removeUserShows } from '../../utils/spotifyApi'
 
-export const ActionBar = ({ onFollow }) => {
-  const [isFollowing, setIsFollowing] = useState()
-  const location = useLocation()
-  const { user } = useAuth()
-  const [{ podcast }, dispatch, actions] = usePlayContext()
+export const ActionBar = () => {
+  const { podcastId } = useParams()
+  const queryClient = useQueryClient()
+  const follow = useMutation(id => addUserShows(id), {
+    onSettled: () => queryClient.invalidateQueries('followStatus')
+  })
+  const unfollow = useMutation(id => removeUserShows(id), {
+    onSettled: () => queryClient.invalidateQueries('followStatus')
+  })
+  const { data, isLoading, isError } = useFollowStatus(podcastId)
+  let isFollowing = data?.[0]
+  // const [{ podcast }, dispatch, actions] = usePlayContext()
 
-  useEffect(() => {
-    if (user) {
-      const paths = location.pathname.split('/')
-      const id = paths[paths.length - 1]
-      const following =
-        user.library?.find(podcast => podcast === id) !== undefined
-      setIsFollowing(following)
+  const handleFollowOnClick = async () => {
+    if (isFollowing) {
+      unfollow.mutate(podcastId)
+    } else {
+      follow.mutate(podcastId)
     }
-  }, [location, user])
-
-  const handlePlayClick = () => {
-    dispatch({ type: actions.QUEUE_PODCAST, payload: podcast.items })
-    dispatch({ type: actions.QUEUE_PLAY })
   }
+
+  const handlePlayClick = () => {}
 
   return (
     <div className='flex items-center h-24 px-8 bg-red-600 bg-gradient-to-t from-true-gray-900 to-semi-transparent'>
@@ -33,7 +37,12 @@ export const ActionBar = ({ onFollow }) => {
           className='p-4 mr-10 rounded-full bg-lime-600 h-14 w-14 text-green-50'
           onPlay={handlePlayClick}
         />
-        {user && <FollowButton isFollowing={isFollowing} onClick={onFollow} />}
+        {!isLoading && !isError && (
+          <FollowButton
+            isFollowing={isFollowing}
+            onClick={handleFollowOnClick}
+          />
+        )}
       </div>
     </div>
   )
