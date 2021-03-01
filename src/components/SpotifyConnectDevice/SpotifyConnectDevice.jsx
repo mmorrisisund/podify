53c9e489e918f69466e8cf849a8d28134cbfdc69
MiMo <michael.a.morris.public@gmail.com>
2021-02-28 22:10:40 -0600

    feat: initial commit - SpotifyConnectDevice
    component to establish connection as Spotify Connect device

---
+++ src/components/SpotifyConnectDevice/SpotifyConnectDevice.jsx
@@ -0,0 +1,77 @@
+import { useEffect } from 'react'
+import { useImportScript } from '../../hooks'
+
+export const SpotifyConnectDevice = ({
+  deviceName,
+  getOAuthToken,
+  volume,
+  onInitializationError,
+  onAuthenticationError,
+  onAccountError,
+  onPlaybackError,
+  onPlayerStateChanged,
+  onReady,
+  onNotReady
+}) => {
+  useImportScript('https://sdk.scdn.co/spotify-player.js')
+
+  useEffect(() => {
+    window.onSpotifyWebPlaybackSDKReady = async () => {
+      const player = new window.Spotify.Player({
+        name: deviceName,
+        getOAuthToken,
+        volume
+      })
+
+      // Error handling
+      player.addListener('initialization_error', params =>
+        onInitializationError?.(params)
+      )
+      player.addListener('authentication_error', params =>
+        onAuthenticationError?.(params)
+      )
+      player.addListener('account_error', params => onAccountError?.(params))
+      player.addListener('playback_error', params => onPlaybackError?.(params))
+
+      // Playback status updates
+      player.addListener('player_state_changed', params =>
+        onPlayerStateChanged?.(params)
+      )
+
+      // Ready
+      player.addListener('ready', async ({ device_id }) => {
+        const accessToken = await player._options.getOAuthToken()
+
+        await fetch('https://api.spotify.com/v1/me/player', {
+          method: 'PUT',
+          headers: {
+            'content-type': 'application/json',
+            authorization: `Bearer ${accessToken}`
+          },
+          body: JSON.stringify({
+            device_ids: [device_id]
+          })
+        })
+        onReady?.(device_id)
+      })
+
+      // Not Ready
+      player.addListener('not_ready', onNotReady)
+
+      player.connect()
+    }
+  }, [
+    deviceName,
+    getOAuthToken,
+    volume,
+    onInitializationError,
+    onAuthenticationError,
+    onAccountError,
+    onPlaybackError,
+    onPlayerStateChanged,
+    onReady,
+    onNotReady
+  ])
+
+  return null
+}
