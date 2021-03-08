350de308cf4a6cb543a111c04a4dd287942fc998
MiMo <michael.a.morris.public@gmail.com>
2021-03-07 20:56:37 -0600

    feat: update to use new icons
    add new icons and styling

--- src/components/NowPlayingBar/VolumeIcon.jsx
+++ src/components/NowPlayingBar/VolumeIcon.jsx
@@ -1,22 +1,25 @@
-import { MdVolumeMute, MdVolumeDown, MdVolumeUp } from 'react-icons/md'
+import { VolumeOff, VolumeLow, VolumeMedium, VolumeHigh } from '../Icons'
 
-export const VolumeIcon = ({ volume, className }) => (
-  <div className='relative w-6 h-6 mr-4 text-gray-300 opacity-50'>
-    <MdVolumeMute
-      className={`absolute inset-y-0 left-0 w-6 h-6  transition-opacity duration-200 ease-out ${
+export const VolumeIcon = ({ volume }) => (
+  <div className='relative w-4 h-4 '>
+    <VolumeOff
+      className={`absolute inset-y-0 left-0 w-4 h-4 transition-opacity duration-200 ease-out ${
         volume === 0 ? 'opacity-1' : 'opacity-0'
       }`}
     />
-    <MdVolumeDown
-      style={{ left: 2 }}
-      className={`absolute inset-y-0 w-6 h-6  transition-opacity duration-200 ease-out ${
-        volume > 0 && volume < 0.5 ? 'opacity-1' : 'opacity-0'
+    <VolumeLow
+      className={`absolute inset-y-0 left-0 w-4 h-4 transition-opacity duration-200 ease-out ${
+        volume > 0 && volume <= 0.333 ? 'opacity-1' : 'opacity-0'
       }`}
     />
-    <MdVolumeUp
-      style={{ left: 3 }}
-      className={`absolute inset-y-0 w-6 h-6 transition-opacity  duration-200 ease-out ${
-        volume > 0.5 ? 'opacity-1' : 'opacity-0'
+    <VolumeMedium
+      className={`absolute inset-y-0 left-0 w-4 h-4 transition-opacity duration-200 ease-out ${
+        volume > 0.333 && volume <= 0.667 ? 'opacity-1' : 'opacity-0'
+      }`}
+    />
+    <VolumeHigh
+      className={`absolute inset-y-0 left-0 w-4 h-4 transition-opacity duration-200 ease-out ${
+        volume > 0.667 ? 'opacity-1' : 'opacity-0'
       }`}
     />
   </div>
