107817dcfba5e9ef678991d1d4ef5c788bddcd89
MiMo <michael.a.morris.public@gmail.com>
2021-03-07 21:28:05 -0600

    feat: connect state to media display

--- src/components/NowPlayingBar/NowPlayingBar.jsx
+++ src/components/NowPlayingBar/NowPlayingBar.jsx
@@ -1,22 +1,22 @@
-import { usePlayContext } from '../../context/PlayContext'
 import { MediaDisplay } from './MediaDisplay'
 import { Player } from '../Player'
+import { useState } from 'react'
 
 export const NowPlayingBar = ({ imageUrl, episode, artist, trackUrl }) => {
-  const [{ podcast, currentEpisode }] = usePlayContext()
+  const [playerState, setPlayerState] = useState()
 
   return (
     <div className='flex justify-between h-full bg-true-gray-900 align-center'>
       <div className='w-1/3'>
         <MediaDisplay
-          image={currentEpisode?.itunes.image}
-          episodeName={currentEpisode?.title}
-          artistName={podcast?.collectionName}
+          image={playerState?.track_window.current_track.album.images[0].url}
+          episodeName={playerState?.track_window.current_track.name}
+          artistName={playerState?.track_window.current_track.artists[0].name}
         />
       </div>
 
       <div className='flex-grow'>
-        <Player url={currentEpisode?.enclosure.url} />
+        <Player onStateChanged={setPlayerState} />
       </div>
     </div>
   )
