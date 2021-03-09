085bd92a2a7d830103844c2bbdfea80b7d5f939d
MiMo <michael.a.morris.public@gmail.com>
2021-03-08 19:27:14 -0600

    feat: connect library page to spotify
    get users shows from spotify library

--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -1,56 +1,18 @@
 import { useEffect, useState } from 'react'
 import { PodcastCard } from '../components/PodcastCard'
-import { useAuth } from '../providers/auth'
-import { getPodcastById } from '../utils/itunesApi'
-import {
-  clearQueue,
-  addPodcastEpisodesToQueue,
-  playQueue,
-  usePlayContext
-} from '../context/PlayContext'
+import { getUserShows } from '../utils'
 
 export const Library = () => {
-  const { user } = useAuth()
   const [podcasts, setPodcasts] = useState([])
-  const [, dispatch] = usePlayContext()
 
   useEffect(() => {
-    async function getPodcasts (urls) {
-      const promises = await Promise.allSettled(urls.map(getPodcastById))
-      const podcasts = promises.reduce((acc, promise) => {
-        if (promise.status === 'fulfilled') {
-          return [...acc, promise.value]
-        }
-        return acc
-      }, [])
-      setPodcasts(podcasts)
-    }
-    if (user?.library) {
-      getPodcasts(user.library)
-    }
-  }, [user])
-
-  const handleOnPlay = id => {
-    const podcast = podcasts.find(p => p.collectionId === id)
-
-    if (!podcast) return
-
-    dispatch(clearQueue())
-    dispatch(addPodcastEpisodesToQueue(podcast))
-    dispatch(playQueue())
-  }
+    getUserShows().then(data => setPodcasts(data.items.map(item => item.show)))
+  }, [])
 
   return (
-    <div className='flex min-h-screen px-8 py-24 bg-true-gray-900'>
-      {podcasts.map(p => (
-        <PodcastCard
-          key={p.collectionId}
-          id={p.collectionId}
-          author={p.artistName}
-          title={p.collectionName}
-          image={p.image.url}
-          onPlay={handleOnPlay}
-        />
+    <div className='flex flex-wrap min-h-screen px-8 py-24 bg-true-gray-900'>
+      {podcasts?.map(podcast => (
+        <PodcastCard podcast={podcast} />
       ))}
     </div>
   )
